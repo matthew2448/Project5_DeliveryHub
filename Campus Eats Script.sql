59ca8526e9111964af6a283c17fc09c04639faf4
Matthew Young <myoung89@uncc.edu>
2021-04-28 14:30:02 -0400

    Database Update

--- Campus Eats Script.sql
+++ Campus Eats Script.sql
@@ -492,7 +492,7 @@ DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Calculate_Overall_Rating_For_Driver`(in id_of_driver int)
 BEGIN
 	SELECT
-    driver_id, AVG(ontime)
+    driver_id, AVG(ontime), AVG(courteous)
 FROM
     `order`,
     deliveryratings
