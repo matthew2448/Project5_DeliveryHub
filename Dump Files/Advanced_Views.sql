74ae56404f46d8ff9d71366ced14c6d6c9d0113b
dixmx <44511540+dixmx@users.noreply.github.com>
2021-05-05 12:30:51 -0400

    update advancedview.sql

--- Dump Files/Advanced_Views.sql
+++ Dump Files/Advanced_Views.sql
@@ -1,14 +1,15 @@
-drop view if exists avg_restaurant_rating;
-create view avg_restaurant_rating as
-select r.rating_id, avg(overall_rating) as avg_ratings
-from ratings as r
-group by rating_id
-order by avg_ratings desc;
-
-drop view if exists order_restaurant;
+drop view  if exists order_restaurants;
 create view order_restaurants as
-select rt.restaurant_id,rt.restaurant_name
+select rt.restaurant_id,rt.restaurant_name,o.delivery_charge
 from restaurant as rt
 inner join orders as o
 on rt.restaurant_id = o.restaurant_id
 order by restaurant_name;
+
+drop view  if exists location_order;
+create view location_order as
+select l.location_id, l.location_name,l.drop_off_point,o.delivery_charge
+from location as l
+inner join orders as o
+on l.location_id = o.location_id
+order by drop_off_point;
